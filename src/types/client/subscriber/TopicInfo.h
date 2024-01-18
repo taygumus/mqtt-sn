24ac152bdbcb7128ed54fa31f167419d3b356e5b
Tayfun Gumus <taygumus95@gmail.com>
2024-01-18 10:59:25 +0100

    delete file

--- src/types/client/subscriber/TopicInfo.h
+++
@@ -1,9 +0,0 @@
-#ifndef TYPES_CLIENT_PUBLISHER_TOPICINFO_H_
-#define TYPES_CLIENT_PUBLISHER_TOPICINFO_H_
-
-struct TopicInfo {
-    std::string topicName = "";
-    ItemInfo* itemInfo = nullptr;
-};
-
-#endif /* TYPES_CLIENT_PUBLISHER_TOPICINFO_H_ */
